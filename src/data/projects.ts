28ec69ba986fd7684bffecb092d5b7a433bdfae2
FrostPixel <bachngocminhchau@gmail.com>
2026-01-14 02:13:41 +0700

    feat: update experience and project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -27,7 +27,24 @@ export const projects: Project[] = [
     image: '/images/projects/attas.png',
     technologies: ['OR-Tools', 'Python', 'C#'],
     githubUrl: 'https://github.com/chaubnmhe153019/SEP490_G14',
-  }
+  },
+  {
+    id: 'crowded-trajectory-clustering',
+    title: 'Utility Solution: Crowded Trajectory Clustering',
+    description: 'A utility solution for clustering crowded trajectories. Patent number: 93595 A 1-2022-07845.',
+    image: '/images/projects/crowdedtrajectoryclustering.png',
+    technologies: [],
+    githubUrl: 'https://ipvietnam.gov.vn/documents/20182/1435339/CB420A1.pdf/73d4da4c-dd42-4940-bcc1-2c082412075a',
+  },
+  {
+    id: 'buried-city',
+    title: 'Buried City',
+    description: 'Get lost in a remote corner, look for the living using an old radio, stare at something blankly while listening to music, try your best to survive—you may have never imagined such scenes but it seems as if they occur every day. In the game, you will be in danger at any time and you need to be courageous and smart enough to survive!',
+    image: '/images/projects/buriedcity.png',
+    technologies: [],
+    githubUrl: 'https://github.com/githubfrostpixel/buried-city-web',
+    demoUrl: 'https://githubfrostpixel.github.io/buriedcity/',
+  },
 ];
 
 export const awards: Award[] = [
@@ -40,5 +57,14 @@ export const awards: Award[] = [
     link: 'https://sangkienatgt.dantri.com.vn/cac-tac-gia-xuat-sac-cua-cuoc-thi-sang-kien-an-toan-giao-thong-viet-nam-2024',
     demoUrl: 'https://youtu.be/zszUS0VMKwA',
     placement: 'first',
+  },
+  {
+    id: 'make-in-vietnam-2025',
+    title: 'Silver Award – Make in Vietnam Digital Technology Products',
+    competition: 'Make in Vietnam Digital Technology Products Awards',
+    year: '2025',
+    description: 'Recognized for outstanding contribution to digital technology products in Vietnam.',
+    link: 'https://vnexpress.net/sau-san-pham-doat-giai-vang-make-in-viet-nam-2025-5000167-p2.html',
+    placement: 'second',
   },
 ];
